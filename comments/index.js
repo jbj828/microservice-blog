62f7b00162da1c79aef8a0bd4ccc06671568660c
Chung Byung Jae <jbj828@naver.com>
2021-01-01 23:34:03 +0900

    chore: add cors handling library

--- comments/index.js
+++ comments/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const commentsByPostId = {};
 
@@ -13,10 +15,10 @@ app.get("/posts/:id/comments", (req, res) => {
 
 app.post("/posts/:id/comments", (req, res) => {
   const commentId = randomBytes(4).toString("hex");
-  const { contents } = req.body;
+  const { content } = req.body;
   const comments = commentsByPostId[req.params.id] || [];
 
-  comments.push({ id: commentId, contents });
+  comments.push({ id: commentId, content });
   commentsByPostId[req.params.id] = comments;
   res.status(201).send(comments);
 });
